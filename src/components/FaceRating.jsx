82bacb650bf3d3837d3d608cde253c6bc62cd35b
Fran Baña <frnbv7@gmail.com>
2024-06-25 14:45:31 +0200

    Cambios Rating

--- src/components/FaceRating.jsx
+++ src/components/FaceRating.jsx
@@ -1,3 +1,73 @@
+// import * as React from "react";
+// import PropTypes from "prop-types";
+// import { styled } from "@mui/material/styles";
+// import Rating from "@mui/material/Rating";
+// import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+// import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
+// import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
+// import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
+// import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+
+// const StyledRating = styled(Rating)(({ theme }) => ({
+//   "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
+//     color: theme.palette.action.disabled,
+//   },
+// }));
+
+// const customIcons = {
+//   1: {
+//     icon: <SentimentVeryDissatisfiedIcon color="error" />,
+//     label: "Very Dissatisfied",
+//   },
+//   2: {
+//     icon: <SentimentDissatisfiedIcon color="error" />,
+//     label: "Dissatisfied",
+//   },
+//   3: {
+//     icon: <SentimentSatisfiedIcon color="warning" />,
+//     label: "Neutral",
+//   },
+//   4: {
+//     icon: <SentimentSatisfiedAltIcon color="success" />,
+//     label: "Satisfied",
+//   },
+//   5: {
+//     icon: <SentimentVerySatisfiedIcon color="success" />,
+//     label: "Very Satisfied",
+//   },
+// };
+
+// function IconContainer(props) {
+//   const { value, ...other } = props;
+//   return <span {...other}>{customIcons[value].icon}</span>;
+// }
+
+// IconContainer.propTypes = {
+//   value: PropTypes.number.isRequired,
+// };
+
+// function calculateRating(popularity) {
+//   if (popularity >= 7000) return 5;
+//   else if (popularity >= 4000) return 4;
+//   else if (popularity >= 1000) return 3;
+//   else if (popularity >= 500) return 2;
+//   else return 1;
+// }
+
+// export default function MovieRating({ popularity }) {
+//   const ratingValue = calculateRating(popularity);
+
+//   return (
+//     <StyledRating
+//       name="highlight-selected-only"
+//       value={ratingValue}
+//       IconContainerComponent={IconContainer}
+//       getLabelText={(value) => customIcons[value].label}
+//       highlightSelectedOnly
+//     />
+//   );
+// }
+
 import * as React from "react";
 import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
@@ -7,10 +77,11 @@ import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied
 import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
 import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+import { useStateContext } from "../context/stateContext";
 
-const StyledRating = styled(Rating)(({ theme }) => ({
+const StyledRating = styled(Rating)(({ theme, darkMode }) => ({
   "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
-    color: theme.palette.action.disabled,
+    color: darkMode ? "gray" : theme.palette.action.disabled,
   },
 }));
 
@@ -47,8 +118,6 @@ IconContainer.propTypes = {
 };
 
 function calculateRating(popularity) {
-  // Normalizar la popularidad al rango de 1 a 5
-  // Puedes ajustar la lógica según cómo quieras que se refleje la popularidad en el rating
   if (popularity >= 7000) return 5;
   else if (popularity >= 4000) return 4;
   else if (popularity >= 1000) return 3;
@@ -57,6 +126,8 @@ function calculateRating(popularity) {
 }
 
 export default function MovieRating({ popularity }) {
+  const { darkMode } = useStateContext(); // Obtener darkMode desde el contexto
+
   const ratingValue = calculateRating(popularity);
 
   return (
@@ -66,6 +137,7 @@ export default function MovieRating({ popularity }) {
       IconContainerComponent={IconContainer}
       getLabelText={(value) => customIcons[value].label}
       highlightSelectedOnly
+      darkMode={darkMode} // Pasar darkMode como prop a StyledRating
     />
   );
 }
